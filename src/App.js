5fc779a17b925a9372f54fdc7e7de0ee9e835cfb
Maxbwee <maxbowen1234@gmail.com>
2021-11-04 16:15:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Inquiry from './components/Inquiry';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+     <Inquiry/>
     </div>
   );
 }
