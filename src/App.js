340c444aaef8bd8a08af7c609fe80673d796c6cf
niikari <pakniiles@gmail.com>
2021-11-04 14:47:15 +0200

    appbar lisätty

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Inquiry from "./components/Inquiry";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
 
   return (
     <div className="App">
+      <AppBar position="static" color="transparent">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Ohjelmistoprojekti kysely
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Inquiry />
     </div>
   );
