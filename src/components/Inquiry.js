095d2452ee18af771d597aed46323ac9ec2d0730
jakko <jaakko.aakula@gmail.com>
2021-11-04 11:19:34 +0200

    ensimmäinen versio

---
+++ src/components/Inquiry.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function Inquiry() {
+
+const [inquiry, setInquiry] = React.useState();
+const [questions, setQuestions] = React.useState(); 
+
+return(
+    <div>
+
+    </div>
+)
+}
---
+++ src/services/question.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+const baseUrl = 'http://localhost:8080/api/questionss'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data);
+  }
+  
+const getById = (id) => {
+    const request = axios.get(`${baseUrl}/${id}`);
+    return request.then(res => res.data);
+}
+
+  const create = newObject => {
+    const request = axios.post(baseUrl, newObject);
+    console.log('newObject on');
+    console.log(newObject);
+    return request.then(response => console.log(response.data));
+  }
+  
+  const deleteObject = id =>  {
+    console.log(`${baseUrl}/${id}`)
+    const request = axios.delete(`${baseUrl}/${id}`);
+    console.log(request.then(response => response.data));
+    return request.then(response => response.data);
+  }
+  
+  const modify = newObject => {
+    const request = axios.put(`${baseUrl}/${newObject.id}`, newObject);
+    return request.then(response => response.data);
+  }
+  
+  export default {getAll, create, deleteObject, modify, getById}
