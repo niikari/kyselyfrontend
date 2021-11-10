26f6f736a5b4b9872a186f8e1c8198760e06bdb1
Juhisaho <ahojuhis@gmail.com>
2021-11-10 20:00:19 +0200

    kaavio

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,51 +1,31 @@
 import React, { useEffect, useState } from "react";
-
 import ReactDOM from "react-dom";
 import Chart from "react-google-charts";
 
 export default function Results(props) {
 
-useEffect(() => {
-    fetchQuestion()
-
-    fetchAnswers()
-}, [])
-
-
-const fetchQuestion = () => {
-        
+  return (
+    <div className="App">
+      <Chart
+        width={'800px'}
+        height={'400px'}
+        chartType="PieChart"
+        loader={<div>Loading Chart</div>}
+        data={[
+            ['Pizza', 'Popularity'],
+            ['Pepperoni', 33],
+            ['Hawaiian', 26],
+            ['Mushroom', 22],
+            ['Sausage', 10], // Below limit.
+            ['Anchovies', 9], // Below limit.
+        ]}
+        options={{
+        title: 'Popularity of Types of Pizza',
+        sliceVisibilityThreshold: 0.05, // 20%
+        }}
+        rootProps={{ 'data-testid': '7' }}
+        />
+    </div>
+  );
 }
 
-const fetchAnswers = () => {
-
-}
-class Results extends React.Component {
-
-  render() {
-    return (
-      <div className="App">
-        <Chart
-  width={'800px'}
-  height={'400px'}
-  chartType="PieChart"
-  loader={<div>Loading Chart</div>}
-  data={[
-    ['Pizza', 'Popularity'],
-    ['Pepperoni', 33],
-    ['Hawaiian', 26],
-    ['Mushroom', 22],
-    ['Sausage', 10], // Below limit.
-    ['Anchovies', 9], // Below limit.
-  ]}
-  options={{
-    title: 'Popularity of Types of Pizza',
-    sliceVisibilityThreshold: 0.05, // 20%
-  }}
-  rootProps={{ 'data-testid': '7' }}
-/>
-      </div>
-    );
-  }
-}
-
-}
